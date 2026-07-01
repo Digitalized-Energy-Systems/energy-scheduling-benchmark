b64f48bc3f3be64ef578f00e59a359dca24d9cb5
jonas <jonas-augustin@gmx.de>
2026-07-01 12:11:15 +0200

    update networks

--- src/energy_scheduling_benchmark/networks/print_network.py
+++ src/energy_scheduling_benchmark/networks/print_network.py
@@ -9,3 +9,7 @@ print(network.components)
 with pd.option_context("display.max_rows", 100, "display.max_columns", 100):
     print(network.storage_units)
 
+## How to create new networks
+## enter pixi shell
+## snakemake resources/networks/base_s_5_elec_2020.nc --configfile config/config.elec_2020.yaml -j 10
+
